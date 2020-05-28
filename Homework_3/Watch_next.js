c880d4d91f247faecb2205caa454d329f45ac754
Michela Moro <m.moro2@studenti.unibg.it>
2020-05-28 09:44:52 +0200

    Update Watch_next.js

--- Homework_3/Watch_next.js
+++ Homework_3/Watch_next.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const watch_next_schema = new mongoose.Schema({
     title: String,
-    watch_next_list: Array
+    watch_next_list: [{idx: String, url_watch_next: String}]
 }, { collection: 'tedz_data' });
 
 module.exports = mongoose.model('watch_next', watch_next_schema);
